546c1a740d6b39e219c6312c02b7c51f3dc84de3
Jovanni <jovannis8@nycstudents.net>
2024-09-30 21:36:18 -0400

    Comments

--- Sanchez_Jovanni_Lab_3.cpp
+++ Sanchez_Jovanni_Lab_3.cpp
@@ -1,5 +1,5 @@
 // Jovanni Sanchez
-// Lab 3 
+// Lab 3
 // September 30, 2024
 
 #include <iostream>
@@ -7,46 +7,65 @@
 using namespace std;
 
 /* Lab 3: Input/Output Manipulators */
-/* 
-We have to use setw() and setfill() in order to recreate the box
-from Lab 1. setw() and setfill() work similarly to water in a cointainer where
-it will take up as much space as you give it. Both commands need a limit to work.
-*/
 
-int main(){
+int main()
+{
 
-    cout << setw(78) << setfill('#') << "#" << endl; 
-    cout << '#' << setw(77) << setfill(' ') << "#" << endl; 
-    // Makes a wall of # 78 characters long till the last #
-    // Makes a 77 character long space in between 2 #
+     cout << setw(78) << setfill('#') << "#" << endl;
+     cout << '#' << setw(77) << setfill(' ') << "#" << endl;
+     // Makes a wall of # 78 characters long till the last #
+     // Makes a 77 character long space in between 2 #
 
-    cout << "# Ways to access the Task Manager on your Windows computer:" 
-         << setw(19) << setfill(' ') 
-         << "#" << endl;
-    // Makes a 19 character long space in between the command and the hastag
+     cout << "# Ways to access the Task Manager on your Windows computer:"
+          << setw(19) << setfill(' ')
+          << "#" << endl;
+     // Makes a 19 character long space in between the command and the hastag
 
-    cout << '#' << setw(77) << setfill(' ') << "#" << endl;
+     cout << '#' << setw(77) << setfill(' ') << "#" << endl;
+     // Makes a 77 character long space in between 2 #
 
-    cout << "#   Press the key combination Ctrl + Shift + Escape" 
-         << setw(27) << setfill(' ') 
-         << "#" << endl;
-    // Makes a 27 character long space in between the command and the hastag
+     cout << "#   Press the key combination Ctrl + Shift + Escape"
+          << setw(27) << setfill(' ')
+          << "#" << endl;
+     // Makes a 27 character long space in between the command and the hastag
 
-    cout << '#' << setw(77) << setfill(' ') << "#" << endl;
+     cout << '#' << setw(77) << setfill(' ') << "#" << endl;
+     // Makes a 77 character long space in between 2 #
 
-    cout << "#   Press the key combination Ctrl + Alt + Delete and select 'Task Manager'" 
-         << setw(3) << setfill(' ') 
-         << "#" << endl;
-    // Makes a 3 character long space in between the command and the hastag lol
+     cout << "#   Press the key combination Ctrl + Alt + Delete and select 'Task Manager'"
+          << setw(3) << setfill(' ')
+          << "#" << endl;
+     // Makes a 3 character long space in between the command and the hastag lol
 
-    cout << '#' << setw(77) << setfill(' ') << "#" << endl;
+     cout << '#' << setw(77) << setfill(' ') << "#" << endl;
+     // Makes a 77 character long space in between 2 #
 
-    cout << "#   Type 'Task Manager' in the Windows Start menu search" 
-         << setw(22) << setfill(' ') 
-         << "#" << endl;
-        
-    cout << '#' << setw(77) << setfill(' ') << "#" << endl;
-    cout << setw(78) << setfill('#') << "#" << endl;
+     cout << "#   Type 'Task Manager' in the Windows Start menu search"
+          << setw(22) << setfill(' ')
+          << "#" << endl;
+     // Makes a 3 character long space in between the command and the hastag lol
 
-    return 0;
+     cout << '#' << setw(77) << setfill(' ') << "#" << endl;
+     cout << setw(78) << setfill('#') << "#" << endl;
+     // Makes a wall of # 78 characters long till the last #
+     // Makes a 77 character long space in between 2 #
+     // This closes the box
+
+     return 0;
 }
+
+/* OUTPUT BELOW */
+
+/*
+##############################################################################
+#                                                                            #
+# Ways to access the Task Manager on your Windows computer:                  #
+#                                                                            #
+#   Press the key combination Ctrl + Shift + Escape                          #
+#                                                                            #
+#   Press the key combination Ctrl + Alt + Delete and select 'Task Manager'  #
+#                                                                            #
+#   Type 'Task Manager' in the Windows Start menu search                     #
+#                                                                            #
+##############################################################################
+*/
